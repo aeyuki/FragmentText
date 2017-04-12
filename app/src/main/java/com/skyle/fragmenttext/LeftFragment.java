cb8d75c12d6ecc47ebb27b70518a3889de533cbf
askyle <askyle@vip.qq.com>
2017-04-12 22:04:28 +0800

    Initial commit

---
+++ app/src/main/java/com/skyle/fragmenttext/LeftFragment.java
@@ -0,0 +1,21 @@
+package com.skyle.fragmenttext;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/4/7 0007.
+ */
+
+public class LeftFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.left_fragment, container, false);
+        return view;
+    }
+}
